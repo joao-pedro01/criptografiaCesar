71ce6259bb0794c6fb8d6ef1f74a8c76379a4e4b
joao-pedro01 <joao_pedro01@terra.com.br>
2025-05-12 21:05:02 -0300

    commit inicial

---
+++ prisma/seed.ts
@@ -0,0 +1,4 @@
+// seed.ts
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+//import jwt from "jsonwebtoken";
+//import { /*dd,*/ verifyJWT } from "../controllers/functions";
+
+const routes = (app: Application) => {
+    // rotas principais
+    app.route('/').get((req: Request, res: Response) => {
+        res.status(200).json({titulo: "Teste"});
+    });
+    app.route('/api/test').get((req: Request,res: Response) => {
+        res.status(200).json({test: 1});
+    });
+
+    // rotas de arquivos externos
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.append('Access-Control-Allow-Origin', '*');
+        res.append('Access-Control-Allow-Headers', '*');
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        next();
+    });
+    app.use(
+        express.json(),
+        express.urlencoded({ extended: true }),
+    );
+};
+export default routes;
